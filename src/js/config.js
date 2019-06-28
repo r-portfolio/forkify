f1468f9f2de6a103e3ccf3012e59228636827d48
Renato Schroepfer <renatolazaroch@gmail.com>
2019-06-28 13:19:33 -0300

    Added likes function

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const proxy = "https://cors-anywhere.herokuapp.com/";
+export const key = "462b1cc8d4f2730081462fbc65136320";
+
+// New Key = bde8ff9795e48bddcd3e525b39ffcf0a
+// https://www.food2fork.com/about/api
